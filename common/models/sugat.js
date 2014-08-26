055ff3dea099ba50328b52f2e549f7305773e678
sugatsamrat <sugat.samrat@gmail.com>
2014-08-26 07:41:14 +0000

    added new files

---
+++ common/models/sugat.js
@@ -0,0 +1,96 @@
+var loopback = require("loopback");
+var app = require('../../server/server');
+var async = require("async");
+ 
+var ds = app.dataSources.db;
+var aggregateModel = ds.getModel ('sugat');
+
+// Hiding the existing remote methods.
+aggregateModel.sharedClass.find('create',true).shared = false;
+aggregateModel.sharedClass.find('upsert',true).shared = false;
+aggregateModel.sharedClass.find('exists',true).shared = false;
+aggregateModel.sharedClass.find('findById',true).shared = false;
+aggregateModel.sharedClass.find('find',true).shared = false;
+aggregateModel.sharedClass.find('findOne',true).shared = false;
+aggregateModel.sharedClass.find('updateAll',true).shared = false;
+aggregateModel.sharedClass.find('deleteById',true).shared = false;
+aggregateModel.sharedClass.find('count',true).shared = false;
+aggregateModel.sharedClass.find('updateAttributes',false).shared = false;
+
+// Adding a new remote method "testMethod" on Model "Aggregate"
+module.exports = function(sugat) {
+	sugat.testMethod = function(cb) {
+
+		function callRestService (serviceName, cb) {
+			var searchResults = [];
+
+			if(serviceName == 'Product') {
+				var dsProduct = app.dataSources.wcsProductDS;
+				var productModel = dsProduct.getModel('Product');
+
+				productModel.getProductById('12129', function(err, Persons){
+					if (err) {
+						console.log("Error occurred: " + err);
+						return;
+					}
+		        	searchResults.push(Persons);
+		        	cb(null, searchResults);
+		        	console.log("1: " + searchResults);
+		        });
+
+			} else if(serviceName == 'Category') {
+				var dsCategory = app.dataSources.wcsProductDS;
+				var categoryModel = dsCategory.getModel('Category');
+
+				categoryModel.getCategoryById('10306', function(err, Categorys){
+					if (err) {
+						console.log("Error occurred: " + err);
+						return;
+					}
+		        	searchResults.push(Categorys);
+		        	cb(null, searchResults);
+		        	console.log("2: " + searchResults);
+		        });
+			}
+		}
+
+		async.parallel([
+			function(cb) {
+				callRestService('Product', cb);
+			},
+
+			function(cb) {
+				callRestService('Category', cb);	
+			}
+
+		], 
+		// Callback function
+		function(err, results) {
+			console.log("final: " + results.length);
+			for(i=0;i<results.length;i++) {
+	            console.log("result: " + results[i]);
+	          }
+			cb(null, results);
+		})
+
+	}
+
+	sugat.remoteMethod(
+		'testMethod',
+		{
+			returns: {arg:'Aggregate', type: 'Object'}
+		}
+	);
+	
+	sugat.testMethod2 = function(cb) {
+		cb(null,"Hi Sugat");
+	}
+	
+	sugat.remoteMethod(
+		'testMethod2',
+		{
+			returns: {arg:'Result', type: 'String'},
+			http: {path: '/sayhi', verb:'get'}
+		}
+	);
+}
